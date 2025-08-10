6cacd73df0de7fa9b6d76d519b7aa8c40321f5a4
dushriane <arianedushime@gmail.com>
2025-08-10 17:49:56 +0200

    target package populated

---
+++ src/main/java/com/housetreasure/HouseTreasureBackendApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.housetreasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HouseTreasureBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HouseTreasureBackendApplication.class, args);
+    }
+}
