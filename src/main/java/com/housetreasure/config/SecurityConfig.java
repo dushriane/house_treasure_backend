8c49d1a528a5445a68d6fd6da3a44cdf6d566933
dushriane <arianedushime@gmail.com>
2025-08-21 21:20:59 +0200

    populated config package files

--- src/main/java/com/housetreasure/config/SecurityConfig.java
+++ src/main/java/com/housetreasure/config/SecurityConfig.java
@@ -1,5 +1,23 @@
 package com.housetreasure.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
-}
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> {})
+            .csrf(csrt -> csrt.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form.disable());
+            // .httpBasic(withDefaults());
+        return http.build();
+
+    }}
